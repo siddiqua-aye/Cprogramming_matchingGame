7054de3c1ba0a8ed61ab7e1672f70656cc1f5359
siddiqua-aye <82129736+siddiqua-aye@users.noreply.github.com>
2021-04-07 22:58:36 -0400

    Add files via upload

---
+++ Ass5_vs/Ass5_vs/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Ass5_vs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnSpin = new System.Windows.Forms.Button();
+            this.btnX1 = new System.Windows.Forms.Button();
+            this.btnX2 = new System.Windows.Forms.Button();
+            this.btnX3 = new System.Windows.Forms.Button();
+            this.btnFlip = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Ass5_vs.Properties.Resources.Watermelon;
+            this.pictureBox1.Location = new System.Drawing.Point(57, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(146, 143);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Ass5_vs.Properties.Resources.Watermelon;
+            this.pictureBox2.Location = new System.Drawing.Point(249, 81);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(146, 143);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Ass5_vs.Properties.Resources.Watermelon;
+            this.pictureBox3.Location = new System.Drawing.Point(444, 81);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(146, 143);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnSpin
+            // 
+            this.btnSpin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSpin.Location = new System.Drawing.Point(632, 133);
+            this.btnSpin.Name = "btnSpin";
+            this.btnSpin.Size = new System.Drawing.Size(83, 53);
+            this.btnSpin.TabIndex = 3;
+            this.btnSpin.Text = "Spin";
+            this.btnSpin.UseVisualStyleBackColor = true;
+            this.btnSpin.Click += new System.EventHandler(this.btnSpin_Click);
+            // 
+            // btnX1
+            // 
+            this.btnX1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnX1.Location = new System.Drawing.Point(99, 246);
+            this.btnX1.Name = "btnX1";
+            this.btnX1.Size = new System.Drawing.Size(52, 41);
+            this.btnX1.TabIndex = 4;
+            this.btnX1.Text = "X";
+            this.btnX1.UseVisualStyleBackColor = true;
+            this.btnX1.Click += new System.EventHandler(this.btnX1_Click);
+            // 
+            // btnX2
+            // 
+            this.btnX2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnX2.Location = new System.Drawing.Point(292, 246);
+            this.btnX2.Name = "btnX2";
+            this.btnX2.Size = new System.Drawing.Size(52, 41);
+            this.btnX2.TabIndex = 5;
+            this.btnX2.Text = "X";
+            this.btnX2.UseVisualStyleBackColor = true;
+            this.btnX2.Click += new System.EventHandler(this.btnX2_Click);
+            // 
+            // btnX3
+            // 
+            this.btnX3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnX3.Location = new System.Drawing.Point(493, 246);
+            this.btnX3.Name = "btnX3";
+            this.btnX3.Size = new System.Drawing.Size(52, 41);
+            this.btnX3.TabIndex = 6;
+            this.btnX3.Text = "X";
+            this.btnX3.UseVisualStyleBackColor = true;
+            this.btnX3.Click += new System.EventHandler(this.btnX3_Click);
+            // 
+            // btnFlip
+            // 
+            this.btnFlip.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFlip.Location = new System.Drawing.Point(219, 338);
+            this.btnFlip.Name = "btnFlip";
+            this.btnFlip.Size = new System.Drawing.Size(224, 60);
+            this.btnFlip.TabIndex = 7;
+            this.btnFlip.Text = "Flip twice/second";
+            this.btnFlip.UseVisualStyleBackColor = true;
+            this.btnFlip.Click += new System.EventHandler(this.btnFlip_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFlip);
+            this.Controls.Add(this.btnX3);
+            this.Controls.Add(this.btnX2);
+            this.Controls.Add(this.btnX1);
+            this.Controls.Add(this.btnSpin);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnSpin;
+        private System.Windows.Forms.Button btnX1;
+        private System.Windows.Forms.Button btnX2;
+        private System.Windows.Forms.Button btnX3;
+        private System.Windows.Forms.Button btnFlip;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
---
+++ Ass5_vs/Ass5_vs/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ass5_vs
+{
+    public partial class Form1 : Form
+    {
+        //int numOfSpins = 0;
+
+        //static int wins = 0;
+        //static int loses = 0;
+
+        //static int finished = 0; // Holds # buttons clicked. If 3, MessageBox prompted.
+
+        //// Hold stopped card contents
+        //static string picture1;
+        //static string picture2;
+        //static string picture3;
+
+        int numOfSpins = 0;
+
+        int wins = 0;
+        int loses = 0;
+
+        static int finished = 0; // Holds # buttons clicked. If 3, MessageBox prompted.
+
+        // Hold stopped card contents
+        string picture1;
+        string picture2;
+        string picture3;
+
+
+
+        Random r = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSpin_Click(object sender, EventArgs e)
+        {
+            if(numOfSpins == 0)
+            {
+                this.Text = "Win:" + wins +  " Lose:" + loses;
+                numOfSpins++;
+            }
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+        }
+
+        //Timer Enabled - Timer's Tick Actions
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Picture1Flip();
+        } // end method timer1_Tick
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Picture2Flip();
+        } // end method timer2_Tick
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            Picture3Flip();
+        } // end method timer3_Tick
+
+        //Random Flipping of 3 PictureBoxes
+        private void Picture1Flip()
+        {
+            int pic1Number = r.Next(1, 3 + 1);
+            switch (pic1Number)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.Watermelon;
+                    picture1 = "Watermelon";
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.Banana;
+                    picture1 = "Banana";
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.Orange;
+                    picture1 = "Orange";
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.Cherry;
+                    picture1 = "Cherry";
+                    break;
+            } // end switch1
+        } // end method Picture1Flip
+
+        private void Picture2Flip()
+        {
+            int pic2Number = r.Next(1, 3 + 1);
+            switch (pic2Number)
+            {
+                case 1:
+                    pictureBox2.Image = Properties.Resources.Watermelon;
+                    picture2 = "Watermelon";
+                    break;
+                case 2:
+                    pictureBox2.Image = Properties.Resources.Banana;
+                    picture2 = "Banana";
+                    break;
+                case 3:
+                    pictureBox2.Image = Properties.Resources.Orange;
+                    picture2 = "Orange";
+                    break;
+                case 4:
+                    pictureBox2.Image = Properties.Resources.Cherry;
+                    picture2 = "Cherry";
+                    break;
+            } // end switch2
+        } // end method Picture2Flip
+
+        private void Picture3Flip()
+        {
+            int pic3Number = r.Next(1, 3 + 1);
+            switch (pic3Number)
+            {
+                case 1:
+                    pictureBox3.Image = Properties.Resources.Watermelon;
+                    picture3 = "Watermelon";
+                    break;
+                case 2:
+                    pictureBox3.Image = Properties.Resources.Banana;
+                    picture3 = "Banana";
+                    break;
+                case 3:
+                    pictureBox3.Image = Properties.Resources.Orange;
+                    picture3 = "Orange";
+                    break;
+                case 4:
+                    pictureBox3.Image = Properties.Resources.Cherry;
+                    picture3 = "Cherry";
+                    break;
+            } // end switch3
+        } // end method Picture3Flip
+
+        //Disabling Timer - Prompt Message Box(DisplayMessageBox), if meet req.
+        private void btnX1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            finished++;
+            DisplayMessageBox();
+        } // end method btnX1_Click
+
+        private void btnX2_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+            finished++;
+            DisplayMessageBox();
+        } // end method btnX2_Click
+
+        private void btnX3_Click(object sender, EventArgs e)
+        {
+            timer3.Enabled = false;
+            finished++;
+            DisplayMessageBox();
+        } // end method btnX3_Click
+
+        //Prompt Message Box(DisplayMessageBox), if meet req.
+        private void DisplayMessageBox()
+        {
+            if ( (finished == 3) && ((picture1 == picture2) && (picture2 == picture3)) )
+            {
+                wins++;
+                this.Text = "Win:" + wins + " Lose:" + loses;
+                finished = 0;
+                MessageBox.Show("You win!!!", "", MessageBoxButtons.OK);
+            } else
+            {
+                if (finished == 3)
+                {
+                    loses++;
+                    this.Text = "Win:" + wins + " Lose:" + loses;
+                    finished = 0;
+                    MessageBox.Show("Try again...", "", MessageBoxButtons.OK);
+                } // end inner if
+            }
+        } // end method DisplayMessageBox
+
+        //Flip Button Inteval Change
+        private void btnFlip_Click(object sender, EventArgs e)
+        {
+            if(timer1.Interval == 1000)
+            {
+                timer1.Interval = 500;
+                timer2.Interval = 500;
+                timer3.Interval = 500;
+                btnFlip.Text = "Flip once/second";
+            } else
+            {
+                if (timer1.Interval == 500)
+                {
+                    timer1.Interval = 1000;
+                    timer2.Interval = 1000;
+                    timer3.Interval = 1000;
+                    btnFlip.Text = "Flip twice/second";
+                } // end inner if
+            }
+        } // end method btnFlip_Click
+
+        //Close Form1 if Double Clicked
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    } // end class Form1
+} // end namespace Ass5_vs
